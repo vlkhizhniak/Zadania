605885d64852f8f95c8ebb627336ad6390c0cb4a
vlkhizhniak <138230371+vlkhizhniak@users.noreply.github.com>
2023-09-18 12:44:02 +0200

    1-projekt rejestracja

--- Projekty/1-projekt-backend-rejestracja/app/controllers/eventcontroller.js
+++ Projekty/1-projekt-backend-rejestracja/app/controllers/eventcontroller.js
@@ -12,27 +12,20 @@ module.exports = {
             })
     },
     add: (req, res) => {
-        if (req.body.name === '') {
-            res.render('home', {
-                noname: true
-            })
+        if (req.body.name === "" || req.body.event === "---" || req.body.city === "---") {
+            Event.find()
+                .lean()
+                .then((eventlist) => {
+                    res.render("home", {
+                        eventlist: eventlist,
+                        nofield: true,
+                    });
+                })
+        } else {
+            const newEvent = new Event(req.body);
+            newEvent.save();
+            res.redirect("/");
         }
-        if (req.body.event === '---') {
-            res.render('home', {
-                noevent: true
-            })
-        }
-        if (req.body.city === '---') {
-            res.render('home', {
-                nocity: true
-            })
-        }
-        const newEvent = new Event(req.body);
-        newEvent.save()
-            // .catch((err) => {
-            //     res.send(err)
-            // })
-        res.redirect('/')
     },
     delete: (req, res) => {
         Event.findByIdAndDelete(req.params.id)
