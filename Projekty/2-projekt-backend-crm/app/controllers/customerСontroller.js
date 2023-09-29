03d26b38eaee88ee20743fed504efff173301bdd
vlkhizhniak <138230371+vlkhizhniak@users.noreply.github.com>
2023-09-29 10:33:21 +0200

    addCustomer validation

--- Projekty/2-projekt-backend-crm/app/controllers/customerСontroller.js
+++ Projekty/2-projekt-backend-crm/app/controllers/customerСontroller.js
@@ -18,7 +18,21 @@ module.exports = {
         newcustomer.save().then(() => {
             res.redirect('/customer')
         }).catch((err) => {
-            res.send(err)
+            if (err.name === 'ValidationError') {
+                for (const validationError of Object.values(err.errors)) {
+                    if (validationError.path === 'address.zip') {
+                        res.render('customers/add', { zipError: true });
+                        return;
+                    } else if (validationError.path === 'nip') {
+                        res.render('customers/add', { nipError: true });
+                        return;
+                    }
+                    else {
+                        res.render('customers/add', { requiredError: true });
+                        return;
+                    }
+                }
+            }
         })
     },
     delete: async (req, res) => {
@@ -46,20 +60,20 @@ module.exports = {
                     })
             });
     },
-        edit: (req, res) => {
-            Customer.findById(req.params.id)
-                .then((onecustomer) => {
-                    // console.log(onecustomer)
-                    res.render('customers/editcustomer', onecustomer)
-                })
-        },
-            update: (req, res) => {
-                Customer.findByIdAndUpdate(req.params.id, req.body)
-                    .then((onecustomer) => {
-                        res.redirect("/customer/" + onecustomer._id)
-                    })
-                    .catch((err) => {
-                        res.send(err);
-                    });
-            }
+    edit: (req, res) => {
+        Customer.findById(req.params.id)
+            .then((onecustomer) => {
+                // console.log(onecustomer)
+                res.render('customers/editcustomer', onecustomer)
+            })
+    },
+    update: (req, res) => {
+        Customer.findByIdAndUpdate(req.params.id, req.body)
+            .then((onecustomer) => {
+                res.redirect("/customer/" + onecustomer._id)
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    }
 }
