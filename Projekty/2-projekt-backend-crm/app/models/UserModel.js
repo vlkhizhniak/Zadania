4b20e7d7525a03eb5bdfc9c7e3f8dca7224b4fe2
vlkhizhniak <138230371+vlkhizhniak@users.noreply.github.com>
2023-10-08 20:12:40 +0200

    123

--- Projekty/2-projekt-backend-crm/app/models/UserModel.js
+++ Projekty/2-projekt-backend-crm/app/models/UserModel.js
@@ -2,46 +2,55 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true,
-            unique: true,
-            required: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'invalid email']
-        },
-        password: { type: String, required: true,
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'invalid password'] }
-    });
+const User = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "invalid email"],
+  },
+  password: {
+    type: String,
+    required: true,
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "invalid password",
+    ],
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 User.pre("save", function (next) {
-    const user = this;
+  const user = this;
 
-    if (!user.isModified("password")) {
-        return next();
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      res.send(err);
     }
 
-    bcrypt.genSalt(10, function (err, salt) {
-        if (err) {
-            res.send(err);
-        }
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        res.send(err);
+      }
 
-        bcrypt.hash(user.password, salt, function (err, hash) {
-            if (err) {
-                res.send(err);
-            }
-
-            user.password = hash;
-            next();
-        });
+      user.password = hash;
+      next();
     });
+  });
 });
 
 User.methods.generateAuthToken = (user) => {
-    const token = jwt.sign({ _id: user._id }, "secretKey", { expiresIn: "1h" });
-    return token;
+  const token = jwt.sign({ _id: user._id }, "secretKey", { expiresIn: "1h" });
+  return token;
 };
 
 module.exports = mongoose.model("User", User);
